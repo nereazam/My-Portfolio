1453d0870cebe995c695d156d60aebb46dd95255
nereazam <nerea_zama@hotmail.com>
2023-03-07 14:13:26 +0100

    add dark mode

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
-// Seleccionar todos los elementos <a> en la lista de navegación
-var navLinks = document.querySelectorAll(".nav-links a");
-
-// Iterar a través de los elementos <a> y agregar un controlador de eventos "click"
-navLinks.forEach(function (navLink) {
-  navLink.addEventListener("click", function (event) {
-    event.preventDefault(); // Evitar que el enlace navegue a una nueva página
-    select(this); // Llamar a la función "select" con el elemento <a> actual como argumento
-  });
+const nightModeToggle = document.querySelector(".night-mode");
+nightModeToggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
 });
 
-// Función "select" para resaltar el elemento de la lista de navegación seleccionado
-function select(link) {
-  // Deseleccionar todos los elementos <a> de la lista de navegación
-  navLinks.forEach(function (navLink) {
-    navLink.classList.remove("selected");
-  });
+const title = document.getElementById("title");
+let index = 0;
+let text = "Welcome to my website";
 
-  // Seleccionar el elemento <a> actual y agregar la clase "selected"
-  link.classList.add("selected");
+function typeWriter() {
+  if (index < text.length) {
+    title.innerHTML += text.charAt(index);
+    index++;
+    setTimeout(typeWriter, 100);
+  }
 }
+
+window.onload = typeWriter;
+
+const navLinks = document.querySelector(".nav-links");
+const burgerMenu = document.querySelector(".burger-menu");
+const closeMenu = document.querySelector(".close-menu");
+
+burgerMenu.addEventListener("click", () => {
+  navLinks.classList.toggle("nav-active");
+  burgerMenu.classList.toggle("toggle");
+});
+
+closeMenu.addEventListener("click", () => {
+  navLinks.classList.remove("nav-active");
+  burgerMenu.classList.remove("toggle");
+});
